0bc64003a786c6046128249d95f282a75ba6ebdd
CampeloRoberto <142457717+CampeloRoberto@users.noreply.github.com>
2024-03-06 23:33:12 +0000

    aula funcao 106/03/2024

---
+++ aulas/funcao1.c
@@ -0,0 +1,43 @@
+// funcoes void , nao entra nada nem sai nada
+// funcoes que apenas entram dadso e nao sai nada
+// funcoes que entream dados e saem dados
+
+
+
+#include <stdio.h>
+
+void soma3(){
+
+//pode te aparecer um print, mas ela opera o que tem que operar sem retornar nada para o programa principal
+
+}
+
+//int soma2(int quico, int tico){
+
+  
+//}
+
+
+
+
+int soma ( int a, int b){
+
+    int c;
+    c = a+b;
+    printf("Resultado = %d", c);
+return c;
+}
+
+int main()
+{
+    int x = 2;
+    int y = 3;
+  int z;
+    
+
+
+    z = soma (x,y);
+  printf("\n");
+    printf("o Valor de c é %d",z);
+    return 0;
+}
